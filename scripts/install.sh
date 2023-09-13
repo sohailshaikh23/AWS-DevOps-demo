8395894a1f07eb83324dc03f8c1d5e223d6c4ada
sohailshaikh23 <136561120+sohailshaikh23@users.noreply.github.com>
2023-09-13 16:36:04 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,4 +5,5 @@ sudo apt install wget
 cd /home/ubuntu
 wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
 chmod +x ./install
+sudo ./install auto
 sudo service codedeploy-agent status
