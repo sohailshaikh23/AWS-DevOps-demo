cb8b01bef6bc96fddd459477687d3781604f4182
sohailshaikh23 <136561120+sohailshaikh23@users.noreply.github.com>
2023-09-13 16:34:54 +0530

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install ruby-full
+sudo apt install wget
+cd /home/ubuntu
+wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo service codedeploy-agent status
